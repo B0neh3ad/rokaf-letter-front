48050a7af2b6b5dce2c05909cf7c52f0746d9a43
B0neh3ad <js1044k@gmail.com>
2024-03-18 00:22:24 +0900

    Modify login layout

--- rokaf-letter-front/src/components/Login.tsx
+++ rokaf-letter-front/src/components/Login.tsx
@@ -3,6 +3,8 @@ import { FormEventHandler, useState } from "react";
 import { useUserContext } from "../contexts/UserContext";
 import { Link, Navigate } from 'react-router-dom';
 
+import styles from "../styles/login.module.css";
+
 function Login() {
     const [emailInput, setEmailInput] = useState("");
     const [passwordInput, setPasswordInput] = useState("");
@@ -23,36 +25,51 @@ function Login() {
             password: passwordInput
         };
 
-        axios.post("http://localhost:8000/accounts/login/", loginForm)
-        .then((res) => {
-            if (res.status === 200) {
-                setToken(res.data.token);
-            }
-        })
-        .catch((err) => {
-            switch (err.response.status) {
-                case 401:
-                    alert('이메일 또는 비밀번호가 잘못되었습니다.');
-                    break;
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/accounts/login/`, loginForm)
+            .then((res) => {
+                if (res.status === 200) {
+                    setToken(res.data.token);
+                }
+            })
+            .catch((err) => {
+                switch (err.response.status) {
+                    case 401:
+                        alert('이메일 또는 비밀번호가 잘못되었습니다.');
+                        break;
 
-                default:
-                    break;
-            }
-        });
+                    default:
+                        break;
+                }
+            });
     }
 
     return (
         <>
-            { token && <Navigate to='/' /> }
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">E-mail </label>
-                <input type="email" name="email" value={emailInput} onChange={(e) => setEmailInput(e.target.value)}/>
-                <br />
-                <label htmlFor="password">Password </label>
-                <input type="password" name="password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} />
-                <br />
-                <button type="submit">로그인</button>
-            </form>
+            {token && <Navigate to='/' />}
+            <div className={styles.formWrapper}>
+                <h1>로그인</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        className={styles.input}
+                        name="email"
+                        placeholder="이메일"
+                        value={emailInput}
+                        onChange={(e) => setEmailInput(e.target.value)}
+                    />
+                    <br />
+                    <input
+                        type="password"
+                        className={styles.input}
+                        name="password"
+                        placeholder="비밀번호"
+                        value={passwordInput}
+                        onChange={(e) => setPasswordInput(e.target.value)}
+                    />
+                    <br />
+                    <button type="submit" className={styles.button}>로그인</button>
+                </form>
+            </div>
             <Link to="/signup">회원가입</Link>
         </>
     );
