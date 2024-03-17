217b59c6a5b688d0660c6236e7248527dea84f2f
B0neh3ad <js1044k@gmail.com>
2024-03-18 00:21:45 +0900

    Add children to main component

--- rokaf-letter-front/src/pages/Router.tsx
+++ rokaf-letter-front/src/pages/Router.tsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../components/Main";
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
+import SentLetters from "../components/SentLetters";
+import Trainees from "../components/Trainees";
 
 const Router = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
+        children: [
+            {
+                path: '/sent',
+                element: <SentLetters />,
+            },
+            {
+                path: '/trainees',
+                element: <Trainees />,
+            }
+        ]
     },
     {
         path: '/login',
